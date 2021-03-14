c9240c20e28db43b05d1a1d143d19ef4d27baca9
karanrawat25 <48472578+karanrawat25@users.noreply.github.com>
2021-03-14 16:47:34 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	for(int i=0;i<n-1;i++)
+	{	
+		int sum=0;
+		for(int j=i;j<n;j++)
+		{
+			sum=sum+a[j];
+			cout<< sum <<endl;
+		}
+	}
+}
+
